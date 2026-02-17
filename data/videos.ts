e3d69a80d00da1798062e47336fba448770bca47
Artemis <dutchnewsru@gmail.com>
2026-02-17 15:31:14 +0200

    data added

---
+++ data/videos.ts
@@ -0,0 +1,46 @@
+
+export interface Video {
+  id: string;
+  title: string;
+  channel: string;
+  views: string;
+}
+
+export const videos: Video[] = [
+  {
+    id: "dQw4w9WgXcQ",
+    title: "Rick Astley - Never Gonna Give You Up",
+    channel: "Rick Astley",
+    views: "1.5B views",
+  },
+  {
+    id: "9bZkp7q19f0",
+    title: "PSY - GANGNAM STYLE",
+    channel: "officialpsy",
+    views: "5B views",
+  },
+  {
+    id: "kJQP7kiw5Fk",
+    title: "Luis Fonsi - Despacito ft. Daddy Yankee",
+    channel: "Luis Fonsi",
+    views: "8.3B views",
+  },
+  {
+    id: "JGwWNGJdvx8",
+    title: "Ed Sheeran - Shape of You",
+    channel: "Ed Sheeran",
+    views: "6.2B views",
+  },
+  {
+    id: "RgKAFK5djSk",
+    title: "Wiz Khalifa - See You Again ft. Charlie Puth",
+    channel: "Wiz Khalifa",
+    views: "6B views",
+  },
+  {
+    id: "fRh_vgS2dFE",
+    title: "Justin Bieber - Sorry",
+    channel: "Justin Bieber",
+    views: "3.6B views",
+  },
+];
