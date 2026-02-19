5a1ad09ec786e360e4d7e367e75c3358763e4f36
Artemis <dutchnewsru@gmail.com>
2026-02-19 17:00:44 +0200

    react dump

---
+++ components/VideoCard.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { Play, ExternalLink } from "lucide-react";
+
+import type { Video } from "@/data/videos";
+
+interface VideoCardProps {
+  video: Video;
+  onPlay: (video: Video) => void;
+}
+
+export function VideoCard({ video, onPlay }: VideoCardProps) {
+  const thumbnailUrl = `https://img.youtube.com/vi/${video.id}/maxresdefault.jpg`;
+  const youtubeUrl = `https://www.youtube.com/watch?v=${video.id}`;
+
+  return (
+  <div onClick={() => onPlay(video)} style={{ cursor: "pointer", marginBottom: "20px" }}>
+    <img
+      src={thumbnailUrl}
+      alt={video.title}
+      style={{ width: "100%", maxWidth: "400px" }}
+    />
+    <h3>{video.title}</h3>
+    <p>{video.channel}</p>
+  </div>
+);
+
+}
