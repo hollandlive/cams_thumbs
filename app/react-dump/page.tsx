e21c1873837c6b844ddc6aab45250aec5e3959af
Artemis <dutchnewsru@gmail.com>
2026-02-21 11:08:07 +0200

    learning react - created first component - MyButton() in react dump subfolder

--- app/react-dump/page.tsx
+++ app/react-dump/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+function MyButton() {
+  function handleClick() {
+    alert("You clicked me!11");
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Click me
+    </button>
+  );
+}
+
 export default function ReactDumpPage() {
   return (
     <div>
       <h1>React Dump</h1>
-      <button onClick={() => alert("Clicked!")}>
-        Click me
-      </button>
+      <MyButton />
     </div>
   );
 }
