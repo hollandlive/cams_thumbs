19b27cf490be70fffc4752e14ec1e9b7f27cea86
Artemis <dutchnewsru@gmail.com>
2026-02-21 09:55:30 +0200

    learning react - subfolder react-dump and page.tsx created

---
+++ app/react-dump/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+export default function ReactDumpPage() {
+  return (
+    <div>
+      <h1>React Dump</h1>
+      <button onClick={() => alert("Clicked!")}>
+        Click me
+      </button>
+    </div>
+  );
+}
