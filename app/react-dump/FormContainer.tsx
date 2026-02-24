0198d3402f6466ebe4aa7a68746c9b6ac30ba450
Artemis <dutchnewsru@gmail.com>
2026-02-24 16:28:02 +0200

    learning react - parrent and child components - created a reusable component TextInput

--- app/react-dump/FormContainer.tsx
+++ app/react-dump/FormContainer.tsx
@@ -2,20 +2,36 @@
 
 import { useState } from "react";
 
-
-function FirstNameInput({ value, onChange }) {
-  return <input value={value} onChange={(e) => onChange(e.target.value)} />;
+function TextInput({ value, onChange, placeholder }: TextInputProps & { placeholder: string }) {
+  return (
+    <input
+      type="text"
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      placeholder={placeholder}
+    />
+  );
 }
 
-function LastNameInput({ value, onChange }) {
-  return <input value={value} onChange={(e) => onChange(e.target.value)} />;
+function Preview({
+  firstName,
+  lastName
+}: {
+  firstName: string;
+  lastName: string;
+}) {
+  return (
+    <p>
+      Hello, {firstName} {lastName}
+    </p>
+  );
 }
 
-function Preview({ firstName, lastName }) {
-  return <p>Hello, {firstName} {lastName}</p>;
-}
-
-function CharacterCounter({ totalLength }) {
+function CharacterCounter({
+  totalLength
+}: {
+  totalLength: number;
+}) {
   return <p>Total characters: {totalLength}</p>;
 }
 
@@ -25,95 +41,50 @@ function FormContainer() {
 
   return (
     <div>
-      <FirstNameInput value={firstName} onChange={setFirstName} />
-      <LastNameInput value={lastName} onChange={setLastName} />
-      <Preview firstName={firstName} lastName={lastName} />
-      <CharacterCounter totalLength={firstName.length + lastName.length} />
-    </div>
+      <TextInput
+        value={firstName}
+        onChange={setFirstName}
+        placeholder="First name"/>
+    <TextInput
+        value={lastName}
+        onChange={setLastName}
+        placeholder="Last name"/>
+    <CharacterCounter totalLength={firstName.length + lastName.length} />
+    <Preview firstName={firstName} lastName={lastName} />
+</div>
   );
 }
 
 export default FormContainer;
 
+// function FirstNameInput({ value, onChange }) {
+//   return <input value={value} onChange={(e) => onChange(e.target.value)} />;
+// }
+
+// function LastNameInput({ value, onChange }) {
+//   return <input value={value} onChange={(e) => onChange(e.target.value)} />;
+// }
+
+// function Preview({ firstName, lastName }) {
+//   return <p>Hello, {firstName} {lastName}</p>;
+// }
+
+// function CharacterCounter({ totalLength }) {
+//   return <p>Total characters: {totalLength}</p>;
+// }
+
 // function FormContainer() {
 //   const [firstName, setFirstName] = useState("");
 //   const [lastName, setLastName] = useState("");
 
-//   function FirstNameInput({
-//   value,
-//   onChange
-// }: {
-//   value: string;
-//   onChange: (val: string) => void;
-// }) {
-//   return (
-//     <input
-//       type="text"
-//       value={value}
-//       onChange={(e) => onChange(e.target.value)}
-//       placeholder="First name"
-//     />
-//   );
-// }
-
-// function LastNameInput({
-//   value,
-//   onChange
-// }: {
-//   value: string;
-//   onChange: (val: string) => void;
-// }) {
-//   return (
-//     <input
-//       type="text"
-//       value={value}
-//       onChange={(e) => onChange(e.target.value)}
-//       placeholder="Last name"
-//     />
-//   );
-// }
-
-// function Preview({
-//   firstName,
-//   lastName
-// }: {
-//   firstName: string;
-//   lastName: string;
-// }) {
-//   return (
-//     <p>
-//       Hello, {firstName} {lastName}
-//     </p>
-//   );
-// }
-
-// function CharacterCounter({
-//   totalLength
-// }: {
-//   totalLength: number;
-// }) {
-//   return <p>Total characters: {totalLength}</p>;
-// }
-
 //   return (
 //     <div>
-//       <FirstNameInput
-//         value={firstName}
-//         onChange={setFirstName}
-//       />
-
-//       <LastNameInput
-//         value={lastName}
-//         onChange={setLastName}
-//       />
-
+//       <FirstNameInput value={firstName} onChange={setFirstName} />
+//       <LastNameInput value={lastName} onChange={setLastName} />
 //       <Preview firstName={firstName} lastName={lastName} />
-
-//       <CharacterCounter
-//         totalLength={firstName.length + lastName.length}
-//       />
+//       <CharacterCounter totalLength={firstName.length + lastName.length} />
 //     </div>
 //   );
 // }
 
-//export default FormContainer;
+// export default FormContainer;
