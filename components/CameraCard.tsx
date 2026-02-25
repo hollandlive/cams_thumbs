6235f385fd716472d85a0ae35f5767acff93884e
Artemis <dutchnewsru@gmail.com>
2026-02-25 09:18:54 +0200

    learning react - MVP - cameras.ts - Model - now CameraCard.tsx - View

--- components/CameraCard.tsx
+++ components/CameraCard.tsx
@@ -1,40 +1,6 @@
-"use client";
+import { Camera } from "@/data/cameras";
 
-const cameras = [
-//   { id: 1, city: "Athens", title: "Syntagma Square", image: "/pic1.jpg" },
-//   { id: 2, city: "Crete", title: "Chania Beach", image: "/pic2.jpg" },
-//   { id: 3, city: "Mykonos", title: "Oia Sunset", image: "/pic3.jpg"  },
-//   { id: 3, city: "Santorini", title: "RDM Sunset", image: "/pic4.jpg"  },
-// ];
-
-const [searchTerm, setSearchTerm] = useState("");
-
-function SearchBar({ value, onChange }) {
-  return (
-    <input
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder="Search by city..."
-    />
-  );
-}
-
-const filteredCameras = cameras.filter((camera) =>
-  camera.city.toLowerCase().includes(searchTerm.toLowerCase())
-);
-
-function CameraList({ cameras }) {
-  return (
-    <div>
-      {cameras.map((camera) => (
-        <CameraCard key={camera.id} camera={camera} />
-      ))}
-    </div>
-  );
-}
-
-function CameraCard({ camera }) {
+export default function CameraCard({ camera }: { camera: Camera }) {
   return (
     <div>
       <img src={camera.image} width="300" />
