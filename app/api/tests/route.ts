d0b90aeca3ac47c24839a2b315bf1edc06cd63e7
Artemis <dutchnewsru@gmail.com>
2026-02-28 16:27:23 +0200

    learning react - day 6 sprint 1 - week 2 - add setLoading and code clean up

--- app/api/tests/route.ts
+++ app/api/tests/route.ts
@@ -3,7 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   const cameras = [
     { id: 1, title: "TEST 1" },
-    { id: 2, title: "TEST 2" }
+    { id: 2, title: "TEST 2" },
+    { id: 3, title: "TEST 3" },
+
   ];
 
   return NextResponse.json(cameras);
--- app/page.tsx
+++ app/page.tsx
@@ -6,32 +6,27 @@ import SearchBar from "@/components/SearchBar";
 import CameraList from "@/components/CameraList";
 
 export default function Page() {
-  // -------------------------
-  // DAY 4: Search & Filter
-  // -------------------------
+  // Day 4 - Search & Filter
   const [searchTerm, setSearchTerm] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const filteredCameras = cameras.filter((camera) =>
     camera.city.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
-  // -------------------------
-  // DAY 5: API Fetch (background)
-  // -------------------------
+  // Day 5 - API Fetch (background)
   const [tests, setTests] = useState([]);
 
   useEffect(() => {
     fetch("/api/tests")
       .then((res) => res.json())
       .then((data) => {
-        console.log("API response:", data); // background log
-        setTests(data); // stored but not rendered
+        console.log("API response:", data);
+        setTests(data);
+        setLoading(false);
       });
   }, []);
 
-  // -------------------------
-  // RENDER
-  // -------------------------
   return (
     <div style={{ padding: "40px" }}>
       <SearchBar value={searchTerm} onChange={setSearchTerm} />
@@ -39,46 +34,3 @@ export default function Page() {
     </div>
   );
 }
-
-
-
-
-
-// import Image from "next/image";
-
-// import { videos } from "@/data/videos";
-// import { VideoCard } from "@/components/VideoCard";
-
-// import { cameras } from "@/data/cameras";
-
-// export default function Home() {
-//   return (
-//     <div>
-
-// {videos.map((camera) => (
-//   <VideoCard
-//     key={camera.id}
-//     video={camera}
-//     onPlay={(v) => console.log(v)}
-//   />
-// ))}
-//     </div>
-//   );
-// }
-
-
-
-// export default function Home() {
-//   return (
-//     <div>
-
-// {videos.map((video) => (
-//   <VideoCard
-//     key={video.id}
-//     video={video}
-//     onPlay={(v) => console.log(v)}
-//   />
-// ))}
-//     </div>
-//   );
-// }
