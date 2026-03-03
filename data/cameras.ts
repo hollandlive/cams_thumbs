462771a3f10c229573ee7e249289dd1497cd86fe
Artemis <dutchnewsru@gmail.com>
2026-03-03 18:44:12 +0200

    Sprint 2 Week 1 days 3-4 utube thumbs adds and slug

--- data/cameras.ts
+++ data/cameras.ts
@@ -3,11 +3,42 @@ export interface Camera {
   city: string;
   title: string;
   image: string;
+    slug: string;
+  youtubeId: string;
 }
 
 export const cameras: Camera[] = [
-  { id: 1, city: "Athens", title: "Syntagma Square", image: "/pic1.jpg" },
-  { id: 2, city: "Crete", title: "Chania Beach", image: "/pic2.jpg" },
-  { id: 3, city: "Mykonos", title: "Oia Sunset", image: "/pic3.jpg" },
-  { id: 4, city: "Santorini", title: "RDM Sunset", image: "/pic4.jpg" },
+
+  {
+    id: 1,
+    title: "Syntagma Square",
+    city: "Athens",
+    image: "/pic1.jpg",
+    slug: "athens-syntagma",
+    youtubeId: "PUT_REAL_YOUTUBE_ID_HERE",
+  },
+  {
+    id: 2,
+    title: "Chania Beach",
+    city: "Chania",
+    image: "/pic2.jpg",
+    slug: "chania-beach",
+    youtubeId: "PUT_REAL_YOUTUBE_ID_HERE",
+  },
+    {
+    id: 3,
+    title: "Oia Beach",
+    city: "Crete",
+    image: "/pic3.jpg",
+    slug: "mykonos-beach",
+    youtubeId: "PUT_REAL_YOUTUBE_ID_HERE",
+  },
+    {
+    id: 4,
+    title: "Chania Beach",
+    city: "Mykonos",
+    image: "/pic4.jpg",
+    slug: "crete-beach",
+    youtubeId: "PUT_REAL_YOUTUBE_ID_HERE",
+  },
 ];
