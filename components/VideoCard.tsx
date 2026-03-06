0c492163719d8448931ada4184cefd3a4f677f54
Artemis <dutchnewsru@gmail.com>
2026-03-06 13:59:42 +0200

    Sprint 2 week 3 fix build - commented out VideoCard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,36 +1,36 @@
-"use client";
+// "use client";
 
-// import { Play, ExternalLink } from "lucide-react";
+// // import { Play, ExternalLink } from "lucide-react";
 
-import { Camera } from "@/data/cameras";
+// import { Camera } from "@/data/cameras";
 
-import type { Video } from "@/data/videos";
+// import type { Video } from "@/data/videos";
 
-interface VideoCardProps {
-  video: Video;
-  onPlay: (video: Video) => void;
-}
+// interface VideoCardProps {
+//   video: Video;
+//   onPlay: (video: Video) => void;
+// }
 
-export function VideoCard({ video, onPlay }: VideoCardProps) {
-  const thumbnailUrl = `https://img.youtube.com/vi/${video.id}/maxresdefault.jpg`;
-  const youtubeUrl = `https://www.youtube.com/watch?v=${video.id}`;
+// export function VideoCard({ video, onPlay }: VideoCardProps) {
+//   const thumbnailUrl = `https://img.youtube.com/vi/${video.id}/maxresdefault.jpg`;
+//   const youtubeUrl = `https://www.youtube.com/watch?v=${video.id}`;
 
-  return (
-    <div
-      onClick={() => onPlay(video)}
-      style={{ cursor: "pointer", marginBottom: "20px" }}
-    >
-      <img
-        src={`https://img.youtube.com/vi/${camera.youtubeId}/maxresdefault.jpg`}
-        alt={camera.title}
-        style={{ width: "100%", maxWidth: "400px" }}
-        onError={(e) => {
-          (e.target as HTMLImageElement).src =
-            `https://img.youtube.com/vi/${video.id}/hqdefault.jpg`;
-        }}
-      />
-      <h3>{video.title}</h3>
-      <p>{video.channel}</p>
-    </div>
-  );
-}
+//   return (
+//     <div
+//       onClick={() => onPlay(video)}
+//       style={{ cursor: "pointer", marginBottom: "20px" }}
+//     >
+//       <img
+//         src={`https://img.youtube.com/vi/${camera.youtubeId}/maxresdefault.jpg`}
+//         alt={camera.title}
+//         style={{ width: "100%", maxWidth: "400px" }}
+//         onError={(e) => {
+//           (e.target as HTMLImageElement).src =
+//             `https://img.youtube.com/vi/${video.id}/hqdefault.jpg`;
+//         }}
+//       />
+//       <h3>{video.title}</h3>
+//       <p>{video.channel}</p>
+//     </div>
+//   );
+// }
